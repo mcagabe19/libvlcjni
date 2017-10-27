mcagabe19/libvlcjni

commit 5a06f68fc4787628e960e6afc00588f3950e774e

Use libc++ as static library
Too many crashes in the LoadLibrary for unknown reasons

libvlc/src/org/videolan/libvlc/LibVLC.java
@@ -151,7 +151,6 @@ public class LibVLC extends VLCObject<LibVLC.Event> {
             return;
         sLoaded = true;
 
-        System.loadLibrary("c++_shared");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD_MR1
                 && Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             try {

commit 9aa99621c859eb5b2a2d541568ddf3824af2c4f8

build: fix ODR violation
This commit fixes the regression introduced by
5a06f68fc4787628e960e6afc00588f3950e774e that switched to the c++ static
runtime and caused exceptions to be uncaught.

As https://developer.android.com/ndk/guides/cpp-support.html#static_runtimes
explains it, one process should *NEVER* link with more than one shared
libraries that link with the c++ static library.

To fix this issue: libmla and libvlc are now static and linked together as
libvlcjni.so with the c++ static library.

libvlc/build.gradle
@@ -9,7 +9,7 @@ android {
     sourceSets {
     main {
         jni.srcDirs = [] // Prevent gradle from building native code with ndk; we have our own Makefile for it.
-        jniLibs.srcDirs = [ 'jni/libs', 'private_libs/libs' ] // Where generated .so files are placed.
+        jniLibs.srcDirs = [ 'jni/libs', 'jni/loader/libs', 'private_libs/libs' ] // Where generated .so files are placed.
         manifest.srcFile 'AndroidManifest.xml'
         java.srcDirs = ['src']
         resources.srcDirs = ['src']

libvlc/jni/Android.mk
@@ -1,9 +1,25 @@
 LOCAL_PATH := $(call my-dir)
+MEDIALIBRARY_JNI_DIR := $(LOCAL_PATH)/../../medialibrary/jni
 
+# libvlc jni static library
 include $(CLEAR_VARS)
-LOCAL_MODULE := libvlc
-ARCH=$(APP_ABI)
-LOCAL_SRC_FILES += libvlcjni-modules.c libvlcjni-symbols.c dummy.cpp
+LOCAL_MODULE    := vlcjni_static
+LOCAL_SRC_FILES := libvlcjni.c
+LOCAL_SRC_FILES += libvlcjni-mediaplayer.c
+LOCAL_SRC_FILES += libvlcjni-vlcobject.c
+LOCAL_SRC_FILES += libvlcjni-media.c libvlcjni-medialist.c libvlcjni-mediadiscoverer.c
+LOCAL_SRC_FILES += libvlcjni-dialog.c
+LOCAL_SRC_FILES += thumbnailer.c
+LOCAL_SRC_FILES += std_logger.c
+LOCAL_C_INCLUDES := $(VLC_SRC_DIR)/include $(MEDIALIBRARY_JNI_DIR) $(LOCAL_PATH)/loader
+
+LOCAL_CFLAGS := -std=c11
+include $(BUILD_STATIC_LIBRARY)
+
+# libvlc dynamic library
+include $(CLEAR_VARS)
+LOCAL_MODULE    := vlcjni
+LOCAL_SRC_FILES := libvlcjni-modules.c libvlcjni-symbols.c
 LOCAL_LDFLAGS := -L$(VLC_CONTRIB)/lib
 LOCAL_LDLIBS := \
 	$(VLC_MODULES) \
@@ -17,31 +33,13 @@ LOCAL_LDLIBS := \
 	-lavcodec -lebml \
 	-llua \
 	-lgcrypt -lgpg-error \
-	$(VLC_LDFLAGS)
+	$(MEDIALIBRARY_LDLIBS) \
+	$(VLC_LDFLAGS) \
+	-llog
 
+LOCAL_WHOLE_STATIC_LIBRARIES := libvlcjni_static libmla
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libvlcjni
-
-LOCAL_SRC_FILES := libvlcjni.c
-LOCAL_SRC_FILES += libvlcjni-mediaplayer.c
-LOCAL_SRC_FILES += libvlcjni-vlcobject.c
-LOCAL_SRC_FILES += libvlcjni-media.c libvlcjni-medialist.c libvlcjni-mediadiscoverer.c
-LOCAL_SRC_FILES += libvlcjni-dialog.c
-LOCAL_SRC_FILES += thumbnailer.c
-LOCAL_SRC_FILES += std_logger.c
-
-LOCAL_LDLIBS := -llog
-LOCAL_C_INCLUDES := $(VLC_SRC_DIR)/include
-
-ARCH=$(APP_ABI)
-
-LOCAL_CFLAGS := -std=c11
-ifeq ($(ARCH), armeabi-v7a)
-	LOCAL_CFLAGS += -DHAVE_ARMEABI_V7A
-endif
-
-LOCAL_SHARED_LIBRARIES:= libvlc
-
-include $(BUILD_SHARED_LIBRARY)
+JNILOADER_INCLUDES := $(LOCAL_PATH)/loader
+$(call import-add-path, $(MEDIALIBRARY_JNI_DIR))
+$(call import-module, .)

libvlc/jni/dummy.cpp
@@ -1,1 +1,0 @@
-/* Force ndk build system to link with c++_shared and install it */

libvlc/jni/libvlcjni.c
@@ -30,7 +30,7 @@
 
 #include <vlc/vlc.h>
 
-#include <jni.h>
+#include "jniloader.h"
 
 #include <android/api-level.h>
 
@@ -39,8 +39,6 @@
 #include "std_logger.h"
 
 struct fields fields;
-
-#define VLC_JNI_VERSION JNI_VERSION_1_2
 
 #define THREAD_NAME "libvlcjni"
 JNIEnv *jni_get_env(const char *name);
@@ -99,14 +97,9 @@ JNIEnv *jni_get_env(const char *name)
 static std_logger *p_std_logger = NULL;
 #endif
 
-jint JNI_OnLoad(JavaVM *vm, void *reserved)
+int VLCJNI_OnLoad(JavaVM *vm, JNIEnv* env)
 {
-    JNIEnv* env = NULL;
-    // Keep a reference on the Java VM.
     myVm = vm;
-
-    if ((*vm)->GetEnv(vm, (void**) &env, VLC_JNI_VERSION) != JNI_OK)
-        return -1;
 
     /* Create a TSD area and setup a destroy callback when a thread that
      * previously set the jni_env_key is canceled or exited */
@@ -327,17 +320,11 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved)
 #undef GET_CLASS
 #undef GET_ID
 
-    LOGD("JNI interface loaded.");
-    return VLC_JNI_VERSION;
+    return 0;
 }
 
-void JNI_OnUnload(JavaVM* vm, void* reserved)
+void VLCJNI_OnUnload(JavaVM *vm, JNIEnv *env)
 {
-    JNIEnv* env = NULL;
-
-    if ((*vm)->GetEnv(vm, (void**) &env, VLC_JNI_VERSION) != JNI_OK)
-        return;
-
     (*env)->DeleteGlobalRef(env, fields.IllegalStateException.clazz);
     (*env)->DeleteGlobalRef(env, fields.IllegalArgumentException.clazz);
     (*env)->DeleteGlobalRef(env, fields.String.clazz);

libvlc/jni/loader/Android.mk
@@ -1,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := jniloader
+LOCAL_SRC_FILES := jniloader.c
+LOCAL_LDFLAGS := -ldl -llog
+include $(BUILD_SHARED_LIBRARY)

libvlc/jni/loader/jniloader.c
@@ -1,0 +1,99 @@
+/*****************************************************************************
+ * jniloader.c
+ *****************************************************************************
+ * Copyright © 2017 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+#include <dlfcn.h>
+#include <stdbool.h>
+#include <android/log.h>
+#include "jniloader.h"
+
+#define LOG_TAG "VLC"
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+static void *handle;
+static bool has_ml;
+
+/* jniloader.so is a shared C library used to receive JNI_OnLoad callback.
+ * Indeed, as JNI_OnLoad is already implemented in VLC core for its internal
+ * configuration, we can't have an other JNI_OnLoad implementation inside the
+ * libvlcjni.so.
+ */
+int JNI_OnLoad(JavaVM* vm, void* reserved)
+{
+    JNIEnv *env;
+
+    if ((*vm)->GetEnv(vm, (void**) &env, VLC_JNI_VERSION) != JNI_OK)
+    {
+        LOGE("GetEnv failed");
+        return -1;
+    }
+
+    handle = dlopen("libvlcjni.so", RTLD_LAZY);
+    if (!handle)
+    {
+        LOGE("could not link libvlcjni.so");
+        return -1;
+    }
+
+    int (*load)(JavaVM *, JNIEnv*);
+    load = dlsym(handle, "VLCJNI_OnLoad");
+    if (!load || load(vm, env) != 0)
+    {
+        if (!load)
+            LOGE("could not find VLCJNI_OnLoad");
+        else
+            LOGE("VLCJNI_OnLoad failed");
+        return -1;
+    }
+
+    /* MediaLibraryJNI_OnLoad is not mandatory */
+    load = dlsym(handle, "MediaLibraryJNI_OnLoad");
+    if (load && load(vm, env) != 0)
+    {
+        LOGE("MediaLibraryJNI_OnLoad failed");
+        return -1;
+    }
+    has_ml = !!load;
+
+    return VLC_JNI_VERSION;
+}
+
+void JNI_OnUnload(JavaVM *vm, void *reserved)
+{
+    JNIEnv* env;
+
+    if ((*vm)->GetEnv(vm, (void**) &env, VLC_JNI_VERSION) != JNI_OK)
+        return;
+    void (*unload)(JavaVM *, JNIEnv*);
+
+    if (has_ml)
+    {
+        unload = dlsym(handle, "MediaLibraryJNI_OnUnload");
+        if (unload)
+            unload(vm, env);
+        else
+            LOGE("could not find MediaLibraryJNI_OnUnload");
+    }
+    unload = dlsym(handle, "VLCJNI_OnUnload");
+    if (unload)
+        unload(vm, env);
+    else
+        LOGE("could not find VLCJNI_OnUnload");
+
+    dlclose(handle);
+}

libvlc/jni/loader/jniloader.h
@@ -1,0 +1,36 @@
+/*****************************************************************************
+ * jniloader.h
+ *****************************************************************************
+ * Copyright © 2017 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+#include <jni.h>
+
+#define VLC_JNI_VERSION JNI_VERSION_1_2
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+int MediaLibraryJNI_OnLoad(JavaVM *vm, JNIEnv *env);
+void MediaLibraryJNI_OnUnload(JavaVM *vm, JNIEnv *env);
+
+int VLCJNI_OnLoad(JavaVM *vm, JNIEnv *env);
+void VLCJNI_OnUnload(JavaVM *vm, JNIEnv *env);
+
+#ifdef  __cplusplus
+}
+#endif

libvlc/src/org/videolan/libvlc/LibVLC.java
@@ -187,8 +187,8 @@ public class LibVLC extends VLCObject<LibVLC.Event> {
         }
 
         try {
-            System.loadLibrary("vlc");
             System.loadLibrary("vlcjni");
+            System.loadLibrary("jniloader");
         } catch (UnsatisfiedLinkError ule) {
             Log.e(TAG, "Can't load vlcjni library: " + ule);
             /// FIXME Alert user